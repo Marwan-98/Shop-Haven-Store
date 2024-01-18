adfe09c4922f0171b668522c638a156388478d31
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2024-01-18 15:16:18 +0200

    Main Nav Setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
+import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 
 const Navbar = () => {
   return (
     <div className="border-b">
         <Container>
-            Navbar
+            <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                    <p className="font-bold text-xl">STORE</p>
+                </Link>
+                <MainNav data={[]}/>
+            </div>
         </Container>
     </div>
   );
