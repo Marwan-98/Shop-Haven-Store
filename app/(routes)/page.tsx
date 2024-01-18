a0cc113ceedcd352663d18cd9f1bd3197d835c85
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2024-01-18 19:44:04 +0200

    Create Products list

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/Billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/Container";
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("54e88f6e-8fbe-4600-a7ff-7db79e119bca");
 
   return (
@@ -13,6 +16,7 @@ const HomePage = async () => {
         <Billboard data={billboard}/>
       </div>
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
