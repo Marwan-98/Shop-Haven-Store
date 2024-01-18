37a703ff4dd17639b826e53b7561ff0d0a3f3c39
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2024-01-18 19:40:03 +0200

    Billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("54e88f6e-8fbe-4600-a7ff-7db79e119bca");
+
   return (
-    <div>HomePage</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+      </div>
+    </Container>
   );
 };
 
