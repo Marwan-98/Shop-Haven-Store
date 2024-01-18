88100917fbf6606e97844fac70ccea87bed79437
Marwan Ashraf <marwan.ashraf@scandiweb.com>
2024-01-18 15:43:52 +0200

    Navbar actions

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ShoppingBag } from "lucide-react";
+import React, { useEffect, useState } from "react";
+
+import Button from "@/components/ui/Button";
+
+const NavbarActions = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+  return (
+    <div className="ml-auto items-center gap-x-4">
+        <Button className="flex items-center rounded-4 bg-black px-4 py-2">
+            <ShoppingBag
+                size={20}
+                color="white"
+            />
+            <span className="ml-2 text-sm font-medium text-white">
+                0
+            </span>
+        </Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
